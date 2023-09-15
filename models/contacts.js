a62458265a2d7f78e6d82bb97c9ff2c28b02569c
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-09-16 00:44:52 +0300

    first commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,72 @@
 // const fs = require('fs/promises')
+const path = require("path");
+const fs = require("fs").promises;
+const contactsPath = path.join(__dirname, "contacts.json");
+// const contactsPath = path.join("models", "contacts.json");
+// console.log(contactsPath)
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+    const jsonContactsRead = await fs.readFile(contactsPath);
+    const contactsReadResult = JSON.parse(jsonContactsRead);
 
-const removeContact = async (contactId) => {}
+    return contactsReadResult;
+}
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+    const allContactsList = await listContacts();
+    const foundContact = allContactsList.find(
+      (contact) => contact.id === contactId
+    );
 
-const updateContact = async (contactId, body) => {}
+    return foundContact || null;
+}
+
+const removeContact = async (contactId) => {
+    const allContactsList = await listContacts();
+    const index = allContactsList.findIndex(
+      (contact) => contact.id === contactId
+    );
+
+    if (index === -1) {
+      return null;
+    }
+
+    const [result] = allContactsList.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
+
+    return result;
+}
+
+const addContact = async (body) => {
+    const allContactsList = await listContacts();
+
+    const newContact = {
+      id: nanoid(),
+      body
+    };
+
+    allContactsList.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
+
+    return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+    const allContactsList = await listContacts();
+    const index = allContactsList.findIndex(
+      (contact) => contact.id === contactId
+    );
+
+    if (index === -1) {
+      return null;
+    }
+  allContactsList[index] = { contactId, ...body };
+    await fs.writeFile(contactsPath, JSON.stringify(allContactsList, null, 2));
+
+    return allContactsList[index];
+}
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,123 @@
 const express = require('express')
+const Joi = require('joi')
+
+// const contacts = require('../../models/contacts')
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require('../../models/contacts');
+
+const { HttpError } = require('../../helpers')
 
 const router = express.Router()
 
+//  Joi schema
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+})
+
+
+//  controller - фунція обробник реквестів
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+      const result = await listContacts();
+    // const result = await contacts.listContacts();
+  res.status(200).json(result)
+  // console.log(result)
+  // res.json({ message: 'template message' })
+  } catch (error) {
+    next(error);  
+}
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    res.status(200).json(result);
+
+    // console.log(req.params)
+    // res.json({ message: 'template message' })
+  } catch (error) {
+      next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    // Валидація даних з використанням схеми
+    const { error } = addSchema.validate(req.body);
+    // Богдан
+    // if (error) {
+    //   throw HttpError(400, error.message);
+    // }
+        if (error) {
+      throw HttpError(400, 'missing required ' + error.details[0].context.key + ' field');
+    }
+    const result = await addContact(req.body);
+
+    // res.status(201).json(result);
+     const response = {
+      id: result.id,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+    };
+
+    res.status(201).json(response);
+  // console.log(req.body)
+} catch (error) {
+      next(error);
+  }
+  // res.json({ message: 'template message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await removeContact(contactId);
+     if (!result) {
+      throw HttpError(404, 'Not found');
+     }
+    res.status(200).json({
+      message: "contact deleted"
+    })
+  } catch (error) {
+      next(error);
+  }
+  // res.json({ message: 'template message' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+     // Перевірка, что req.body не пусте
+    // if (!req.body) {
+      if (Object.keys(req.body).length === 0) {
+      throw HttpError(400, 'missing fields');
+    }
+    // Валидація даних з використанням схеми
+  const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, 'missing required ' + error.details[0].context.key + ' field');
+    }
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+      if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    res.status(200).json(result);
+  }  catch (error) {
+      next(error);
+  }
+  // res.json({ message: 'template message' })
 })
 
 module.exports = router
