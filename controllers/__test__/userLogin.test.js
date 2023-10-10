b758ef5359538b5ec382387b93f4fa10aa9ef34b
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-10-10 18:00:48 +0300

    add test

--- controllers/__test__/userLogin.test.js
+++ controllers/__test__/userLogin.test.js
@@ -9,26 +9,30 @@ const app = require('../../app');
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
+// --------1 спосіб-------------------
 describe('test user login controller', () => {
-  let server;
+  let server = null;
+
   beforeAll(async () => {
-    await mongoose.connect(DB_HOST).then(() => (server = app.listen(PORT)));
+    await mongoose.connect(DB_HOST);
+    server = app.listen(PORT);
   });
 
   afterAll(async () => {
     await mongoose.connection.close();
-    await server.close();
+    server.close();
   });
 
   it('should return user object and token', async () => {
-    const res = await request(app).post('/users/login').send({
+    const loginData = {
       email: "example@example.com",
       password: "examplepassword"
-    });
+    }
+    const {statusCode, body} = await request(app).post('/users/login').send(loginData);
 
-    expect(res.statusCode).toBe(200);
-    expect(res.body.token).toBeTruthy();
-    expect(res.body).toMatchObject({
+    expect(statusCode).toBe(200);
+    expect(body.token).toBeTruthy();
+    expect(body).toMatchObject({
       user: {
         email: expect.any(String),
         subscription: expect.any(String),
@@ -36,6 +40,36 @@ describe('test user login controller', () => {
     });
   });
 });
+
+
+// --------2 спосіб--------------------
+// describe('test user login controller', () => {
+//   let server;
+//   beforeAll(async () => {
+//     await mongoose.connect(DB_HOST).then(() => (server = app.listen(PORT)));
+//   });
+
+//   afterAll(async () => {
+//     await mongoose.connection.close();
+//     await server.close();
+//   });
+
+//   it('should return user object and token', async () => {
+//     const res = await request(app).post('/users/login').send({
+//       email: "example@example.com",
+//       password: "examplepassword"
+//     });
+
+//     expect(res.statusCode).toBe(200);
+//     expect(res.body.token).toBeTruthy();
+//     expect(res.body).toMatchObject({
+//       user: {
+//         email: expect.any(String),
+//         subscription: expect.any(String),
+//       },
+//     });
+//   });
+// });
 
 
 
