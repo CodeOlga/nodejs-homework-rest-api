adfd62611b170746148c7efccce97c60952e6dc8
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-10-17 13:41:37 +0300

    add nodemailer with mailtrap

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,4 +1,77 @@
-// // ------Nodemailer-------------
+// ===================1================================================
+// -------ElasticEmail----працює----------
+
+// const ElasticEmail = require('@elasticemail/elasticemail-client');
+
+// const { ELASTIC_API_KEY, META_EMAIL } = process.env;
+
+// const defaultClient = ElasticEmail.ApiClient.instance;
+
+// const { apikey } = defaultClient.authentications;
+// apikey.apiKey = ELASTIC_API_KEY;
+
+// const api = new ElasticEmail.EmailsApi();
+
+// const sendEmailWrapper = ({to:email, subject}) => {
+//     const sendEmail = ElasticEmail.EmailMessageData.constructFromObject({
+//       Recipients: [
+//         new ElasticEmail.EmailRecipient(email)
+//       ],
+//       Content: {
+//         Body: [
+//           ElasticEmail.BodyPart.constructFromObject({
+//             ContentType: "HTML",
+//             Content: "<strong>Test email</strong>"
+//           })
+//         ],
+//         Subject: subject ?? "Test email",
+//         From: META_EMAIL
+//       }
+//     });
+  
+//     const callback = function(error, data, response) {
+//       if (error) {
+//         console.error(error.message);
+//       } else {
+//         console.log('API called successfully');
+//       }
+//     };
+    
+//   api.emailsPost(sendEmail, callback);
+// }
+
+// module.exports = sendEmailWrapper;
+
+
+// ===================2================================================
+// ------Nodemailer--MAILTRAP---працює--------
+
+const nodemailer = require("nodemailer");
+
+const { MAILTRAP_EMAIL, MAILTRAP_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: MAILTRAP_EMAIL,
+    pass: MAILTRAP_PASSWORD
+  }
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async(data) => {
+    const email = {...data, from: MAILTRAP_EMAIL};
+    return transport.sendMail(email);
+}
+
+
+module.exports = sendEmail;
+
+
+// ===================3================================================
+//  ------Nodemailer--META---не працює--------
 
 // const nodemailer = require("nodemailer");
 // require('dotenv').config(); // - прописано в app.js для всього проекту, тому тут непотрібно
@@ -17,7 +90,6 @@
 // // transport - об'єкт, який буде займатися доставкою пошти
 // const transport = nodemailer.createTransport(nodemailerConfig);
 
-
 // // Універсальна функці sendEmail - отримує об'єкт data (email одержувача, тема листа та вміст), дописує відправника та відправляє email
 // const sendEmail = async(data) => {
 //     const email = {...data, from: META_EMAIL};
@@ -27,8 +99,8 @@
 // module.exports = sendEmail;
 
 
-
-// ------SendGrid-------------
+// ===================4================================================
+// ------SendGrid----не створено акаунт---------
 
 // const sgMail = require('@sendgrid/mail');
 
@@ -37,55 +109,10 @@
 // sgMail.setApiKey{ SENDGRID_API_KEY };
 
 // const sendEmail = async (data) => {
-//   const email = { ...data, from: "olga.krv@gmail.com" };
+//   const email = { ...data, from: "olga.krv@meta.ua" };
 //   await sgMail.send(email);
 //   return true;
 // }
 // module.exports = sendEmail;
-
-
-// -------ElasticEmail--------------
-
-const ElasticEmail = require('@elasticemail/elasticemail-client');
-
-const { ELASTIC_API_KEY, META_EMAIL } = process.env;
-
-const defaultClient = ElasticEmail.ApiClient.instance;
-
-const { apikey } = defaultClient.authentications;
-apikey.apiKey = ELASTIC_API_KEY;
-
-const api = new ElasticEmail.EmailsApi();
-
-const sendEmailWrapper = ({to:email, subject}) => {
-    const sendEmail = ElasticEmail.EmailMessageData.constructFromObject({
-      Recipients: [
-        new ElasticEmail.EmailRecipient(email)
-      ],
-      Content: {
-        Body: [
-          ElasticEmail.BodyPart.constructFromObject({
-            ContentType: "HTML",
-            Content: "<strong>Test email</strong>"
-          })
-        ],
-        Subject: subject ?? "Test email",
-        From: META_EMAIL
-      }
-    });
-  
-    const callback = function(error, data, response) {
-      if (error) {
-        console.error(error.message);
-      } else {
-        console.log('API called successfully');
-      }
-    };
-    
-  api.emailsPost(sendEmail, callback);
-}
-
-module.exports = sendEmailWrapper;
-
 
 
